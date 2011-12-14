d8bad75efb0e4616fceb832f72204cd92dd3d13b
Zed A. Shaw <zedshaw@zedshaw.com>
2011-12-13 16:26:59 -0800

    Bit of shuffling since ex0 doesn't make sense in this.

--- bookgen.py
+++ bookgen.py
@@ -5,7 +5,7 @@ chapters = open("chapters.txt").readlines()
 template = open("template.txt").read()
 
 for i, title in enumerate(chapters):
-    out = template % {'number': i, 'title': title.strip()}
-    with open("ex%d.tex" % i, "w") as f:
+    out = template % {'number': i+1, 'title': title.strip()}
+    with open("ex%d.tex" % (i+1), "w") as f:
         f.write(out)
 
